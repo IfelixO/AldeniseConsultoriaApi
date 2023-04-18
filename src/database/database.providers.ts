edd0b8771dd1a194af256da9647f49edc27dabc0
Israel Oliveira <ifelixl.oliveira@gmail.com>
2023-04-18 16:33:23 -0300

    configurando banco de dados

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -4,12 +4,12 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
       useFactory: async () => await createConnection({
-        type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: '',
-        database: 'gerfin', 
+        type: 'postgres',
+        host: 'ep-aged-waterfall-101157.us-east-2.aws.neon.tech',
+        port: 5432,
+        username: 'IfelixO',
+        password: 'IAbxc0TudMG6',
+        database: 'neondb', 
         entities: [
             __dirname + '/../**/*.entity{.ts,.js}',
         ],
